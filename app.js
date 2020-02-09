cb8695061328f6de0b351521cb53ec5c3a0037ff
Shahid Shaikh <shahid.sk27595@gmail.com>
2020-02-09 19:45:55 +0530

    Added a new page to showcase single movies

--- app.js
+++ app.js
@@ -19,16 +19,28 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
-  // Get Movies
   axios
     .get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.APIkey}&language=en-US&page=1`)
     .then(response => {
       res.render('home', { data: response.data.results });
-      // console.log(response.data.results[0].title);
+      console.log(response.data.results);
     })
     .catch(err => {
       console.log(`Error occured ${err}`);
     });
+});
+
+app.get('/movie/:id', (req, res) => {
+  const movieid = req.params.id;
+  axios
+    .get(`https://api.themoviedb.org/3/movie/${movieid}?api_key=${process.env.APIkey}&language=en-US`)
+    .then(response => {
+      res.render('movie', { data: response.data });
+    })
+    .catch(err => {
+      console.log(`Error occured ${err}`);
+    });
+
 });
 
 app.listen(process.env.PORT, () => {
