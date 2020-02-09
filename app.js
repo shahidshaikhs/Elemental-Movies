904319270f0cae05676d1521bbe9c5e8dddc0df3
Shahid Shaikh <shahid.sk27595@gmail.com>
2020-02-09 15:30:33 +0530

    Remove the old code

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 80, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
 });
